9c50fde34584d0798420bb68bcdecaacc5a0ba60
godud2604 <ees238@naver.com>
2021-09-08 23:42:54 +0900

    🎨 Set up html document and import webFont

--- tomorrow/src/App.js
+++ tomorrow/src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
+import './App.css'
+import './style.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       header
-      </header>
+      <header className="App-header">header</header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
