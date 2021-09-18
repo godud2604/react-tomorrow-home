279474e523dc6ad3883744a8f40db6aafa240ad4
godud2604 <ees238@naver.com>
2021-09-18 22:51:29 +0900

    💄 Implement tag components

--- tomorrow/src/App.js
+++ tomorrow/src/App.js
@@ -1,10 +1,13 @@
 import './App.css'
-import './style.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">header</header>
+    <div className="container">
+      <div className="row">
+        <strong className="tag-red">특가</strong>
+        <strong className="tag-gray">무료배송</strong>
+        <strong className="tag-green">첫 리뷰 두 배 적립</strong>
+      </div>
     </div>
   )
 }
