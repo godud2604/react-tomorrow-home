cd6ac1df6c6c6fd8cc4b8eff8de10ce0480cead9
godud2604 <ees238@naver.com>
2021-09-18 23:01:54 +0900

    💄 Implement avatar components

--- tomorrow/src/App.js
+++ tomorrow/src/App.js
@@ -1,12 +1,24 @@
 import './App.css'
+import avatar from './assets/images/img-user-01.jpg'
 
 function App() {
   return (
     <div className="container">
       <div className="row">
-        <strong className="tag-red">특가</strong>
-        <strong className="tag-gray">무료배송</strong>
-        <strong className="tag-green">첫 리뷰 두 배 적립</strong>
+        <div>
+          <strong className="tag-red">특가</strong>
+          <strong className="tag-gray">무료배송</strong>
+          <strong className="tag-green">첫 리뷰 두 배 적립</strong>
+        </div>
+
+        <div>
+          <div className="avatar-32">
+            <img src={avatar} alt="사딸라 아저씨" />
+          </div>
+          <div className="avatar-24">
+            <img src={avatar} alt="사딸라 아저씨" />
+          </div>
+        </div>
       </div>
     </div>
   )
